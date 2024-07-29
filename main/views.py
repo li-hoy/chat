d6bc03071f2c6bf37cba6f20754363aca0e16b8d
Pavel Likhosherst <pe@likhosherst.ru>
2024-07-29 22:02:38 +0300

    get messages action

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from main.models import Person
+from django.http import JsonResponse
 
 def experiments(request):
     try:
@@ -19,3 +20,19 @@ def chat(request):
     }
 
     return render(request, 'chat.html', context)
+
+def messages(request):
+    return JsonResponse({
+        'messages': [
+            {
+                'author_id': 0,
+                'interlocutor_id': 1,
+                'content': 'my message 1',
+            },
+            {
+                'author_id': 1,
+                'interlocutor_id': 0,
+                'content': 'his message 1',
+            },
+        ]
+    })
