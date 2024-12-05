344f7f5e41af7be3e65df6938f0b3e04864810ed
li-hoy <lihosherstpe@yandex.ru>
2024-12-05 22:04:10 +0300

    upgrade 2

--- users/forms/register_form.py
+++ users/forms/register_form.py
@@ -4,11 +4,12 @@ from django.contrib.auth import get_user_model
 class RegistrationUserForm(forms.ModelForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
     class Meta:
         model = get_user_model()
         fields = ['username', 'password']
-        labels = {
-            'first_name': 'Имя',
-            'last_name': 'Фамилия',
-        }
+        # labels = {
+        #     'first_name': 'Имя',
+        #     'last_name': 'Фамилия',
+        # }
